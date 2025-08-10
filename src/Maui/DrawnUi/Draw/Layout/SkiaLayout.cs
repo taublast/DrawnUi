fe032b00d41ef6172c89987a744bd3db3699083b
Nick Kovalsky <taublast@gmail.com>
2025-08-10 18:20:20 +0300

    InvalidateAllPooledCells

--- src/Maui/DrawnUi/Draw/Layout/SkiaLayout.cs
+++ src/Maui/DrawnUi/Draw/Layout/SkiaLayout.cs
@@ -603,40 +603,9 @@ namespace DrawnUi.Draw
 
         protected bool templatesInvalidated;
 
-        public override void InvalidateWithChildren()
-        {
-            if (ChildrenFactory == null)
-                return;
-
-            if (IsTemplated)
-            {
-                //todo cannot really spam this if we find out we need this need to find some
-                //more optimizations to minimize the lag from recreating templates
-                //ChildrenFactory.TemplatesInvalidated = true;
-
-                OnItemSourceChanged();
-            }
-
-            base.InvalidateWithChildren();
-        }
-
-        //protected override void OnChildAdded(SkiaControl child)
-        //{
-        //    base.OnChildAdded(child);
-
-        //    needUpdateViews = true;
-        //}
-
-        //protected override void OnChildRemoved(SkiaControl child)
-        //{
-        //    base.OnChildRemoved(child);
-
-        //    needUpdateViews = true;
-        //}
-
         public override void InvalidateViewsList()
         {
-            base.InvalidateViewsList(); //_orderedChildren = null;
+            base.InvalidateViewsList(); 
 
             ActualizeSubviews();
         }
@@ -863,6 +832,16 @@ namespace DrawnUi.Draw
 
                 if (!CheckAndSetupIfEmpty())
                 {
+                    if (IsTemplated) //fix threads conflict when templates are initialized in background thread
+                    {
+                        var canMeasureTemplates = ChildrenFactory.TemplatesAvailable || force;
+
+                        if (!canMeasureTemplates)
+                            return ScaledSize.CreateEmpty(request.Scale);
+
+                        ChildrenFactory.InvalidateAllPooledCells();
+                    }
+
                     switch (Type)
                     {
                         case LayoutType.Absolute:
@@ -878,17 +857,12 @@ namespace DrawnUi.Draw
                         case LayoutType.Row:
                             if (IsTemplated) //fix threads conflict when templates are initialized in background thread
                             {
-                                var canMeasureTemplates = ChildrenFactory.TemplatesAvailable || force;
-
-                                if (!canMeasureTemplates)
-                                    return ScaledSize.CreateEmpty(request.Scale);
-
                                 if (MeasureItemsStrategy == MeasuringStrategy.MeasureVisible)
                                 {
                                     ContentSize = MeasureList(constraints.Content, request.Scale);
                                     break;
                                 }
-
+                                
                                 ContentSize = MeasureStackTemplated(constraints.Content, request.Scale);
                             }
                             else
@@ -899,14 +873,6 @@ namespace DrawnUi.Draw
                             break;
 
                         case LayoutType.Wrap:
-                            if (IsTemplated) //fix threads conflict when templates are initialized in background thread
-                            {
-                                var canMeasureTemplates = ChildrenFactory.TemplatesAvailable || force;
-
-                                if (!canMeasureTemplates)
-                                    return ScaledSize.CreateEmpty(request.Scale);
-                            }
-
                             ContentSize = MeasureWrap(constraints.Content, request.Scale);
                             break;
 
