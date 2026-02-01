a8bb311dc4a8148315caeb5984923f41333585d0
Nick Kovalsky <taublast@gmail.com>
2026-02-01 10:21:48 +0300

    skiaplayer
    not working yet

---
+++ src/Maui/Samples/Player/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PlayerTests
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => PlayerTests.MauiProgram.CreateMauiApp();
+    }
+}
