90516d5bad59ad9f7fb1efb40e4c64a245c5708a
Nick Kovalsky <taublast@gmail.com>
2026-02-21 16:39:29 +0300

    move audio notes sample to standalone repo

--- src/Maui/Samples/MusicNotes/DeleteBinObj.ps1
+++
@@ -1,4 +0,0 @@
-Get-ChildItem .\ -include bin,obj -Recurse | ForEach-Object ($_) { Remove-Item $_.FullName -Force -Recurse }
-
-Write-Host "Press any key to continue..."
-$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
