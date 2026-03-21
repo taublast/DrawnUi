ebb54db93f5599660e48750d93138467b5f64112
Nick Kovalsky <taublast@gmail.com>
2026-03-21 10:00:23 +0300

    add lock to LimitedQueue

--- src/Shared/Internals/Models/LimitedQueue.cs
+++ src/Shared/Internals/Models/LimitedQueue.cs
@@ -3,19 +3,35 @@
     public class LimitedQueue<T>
     {
         private Queue<T> queue = new();
+        private readonly object _lock = new();
         private readonly int _maxLength = 3;
         private bool _locked;
 
-        public int Count => queue.Count;
+        public int Count
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    return queue.Count;
+                }
+            }
+        }
 
         public T[] ToArray()
         {
-            return queue.ToArray();
+            lock (_lock)
+            {
+                return queue.ToArray();
+            }
         }
 
         public List<T> ToList()
         {
-            return queue.ToArray().ToList();
+            lock (_lock)
+            {
+                return queue.ToArray().ToList();
+            }
         }
 
         public LimitedQueue()
@@ -34,14 +50,17 @@
 
         public void Push(T item)
         {
-            if (_locked)
-                return;
-
-            queue.Enqueue(item);
-            while (queue.Count > _maxLength)
+            lock (_lock)
             {
-                queue.TryDequeue(out var removedItem);
-                OnAutoRemovingItem(removedItem);
+                if (_locked)
+                    return;
+
+                queue.Enqueue(item);
+                while (queue.Count > _maxLength)
+                {
+                    queue.TryDequeue(out var removedItem);
+                    OnAutoRemovingItem(removedItem);
+                }
             }
         }
 
@@ -49,33 +68,48 @@
         {
             get
             {
-                return _locked;
+                lock (_lock)
+                {
+                    return _locked;
+                }
             }
         }
 
         public void Lock()
         {
-            _locked = true;
+            lock (_lock)
+            {
+                _locked = true;
+            }
         }
 
         public void Unlock()
         {
-            _locked = false;
+            lock (_lock)
+            {
+                _locked = false;
+            }
         }
 
         public T Pop()
         {
-            T latestItem;
-            queue.TryDequeue(out latestItem);
-            return latestItem;
+            lock (_lock)
+            {
+                T latestItem;
+                queue.TryDequeue(out latestItem);
+                return latestItem;
+            }
         }
 
         public void Clear()
         {
-            while (queue.Count > 0)
+            lock (_lock)
             {
-                queue.TryDequeue(out var removedItem);
-                OnAutoRemovingItem(removedItem);
+                while (queue.Count > 0)
+                {
+                    queue.TryDequeue(out var removedItem);
+                    OnAutoRemovingItem(removedItem);
+                }
             }
         }
     }
