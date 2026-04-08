31d29022714481f3ecf449f7f2f8e4f9aaeee109
Nick Kovalsky <taublast@gmail.com>
2026-04-08 13:39:44 +0300

    nop

---
+++ src/Blazor/DrawnUi/Draw/BindableObject.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DrawnUi.Draw
+{
+    public class BindableObject : INotifyPropertyChanged
+    {
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+#if DEBUG
+            //       dynamic value = Reflection.GetPropertyValueFor(this, propertyName);
+            //       Console.WriteLine($"[PropertyChanged] BasePage {propertyName} = {value}");
+#endif
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+        #endregion
+
+        public object GetValue(BindableProperty clearColorProperty)
+        {
+            throw new NotImplementedException();
+        }
+        public void SetValue(BindableProperty clearColorProperty, object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BindableProperty           
+    {
+        public static BindableProperty Create(string clearColorName, Type type, Type type1, object defaultValue,
+            Action<BindableObject, object, object> propertyChanged)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
