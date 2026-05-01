93547ac1739bcbf893f894b6524301b23d9f02be
AppoMobi <taublast@gmail.com>
2026-05-01 14:57:53 +0300

    add more colors

--- src/Blazor/DrawnUi/Internals/Core/Colors.cs
+++ src/Blazor/DrawnUi/Internals/Core/Colors.cs
@@ -2,19 +2,163 @@ namespace DrawnUi.Draw
 {
     public static class Colors
     {
+        public static Color Empty { get; } = Color.FromSKColor(SKColors.Empty);
+
         public static Color Transparent { get; } = Color.FromSKColor(SKColors.Transparent);
+
+        public static Color AliceBlue { get; } = Color.FromSKColor(SKColors.AliceBlue);
+
+        public static Color AntiqueWhite { get; } = Color.FromSKColor(SKColors.AntiqueWhite);
+
+        public static Color Aqua { get; } = Color.FromSKColor(SKColors.Aqua);
+
+        public static Color Aquamarine { get; } = Color.FromSKColor(SKColors.Aquamarine);
+
+        public static Color Azure { get; } = Color.FromSKColor(SKColors.Azure);
+
+        public static Color Beige { get; } = Color.FromSKColor(SKColors.Beige);
+
+        public static Color Bisque { get; } = Color.FromSKColor(SKColors.Bisque);
 
         public static Color White { get; } = Color.FromSKColor(SKColors.White);
 
+        public static Color BlanchedAlmond { get; } = Color.FromSKColor(SKColors.BlanchedAlmond);
+
+        public static Color Blue { get; } = Color.FromSKColor(SKColors.Blue);
+
+        public static Color BlueViolet { get; } = Color.FromSKColor(SKColors.BlueViolet);
+
         public static Color Black { get; } = Color.FromSKColor(SKColors.Black);
+
+        public static Color Brown { get; } = Color.FromSKColor(SKColors.Brown);
+
+        public static Color BurlyWood { get; } = Color.FromSKColor(SKColors.BurlyWood);
+
+        public static Color CadetBlue { get; } = Color.FromSKColor(SKColors.CadetBlue);
+
+        public static Color Chartreuse { get; } = Color.FromSKColor(SKColors.Chartreuse);
+
+        public static Color Chocolate { get; } = Color.FromSKColor(SKColors.Chocolate);
+
+        public static Color Coral { get; } = Color.FromSKColor(SKColors.Coral);
+
+        public static Color CornflowerBlue { get; } = Color.FromSKColor(SKColors.CornflowerBlue);
+
+        public static Color Cornsilk { get; } = Color.FromSKColor(SKColors.Cornsilk);
+
+        public static Color Crimson { get; } = Color.FromSKColor(SKColors.Crimson);
+
+        public static Color Cyan { get; } = Color.FromSKColor(SKColors.Cyan);
+
+        public static Color DarkBlue { get; } = Color.FromSKColor(SKColors.DarkBlue);
+
+        public static Color DarkCyan { get; } = Color.FromSKColor(SKColors.DarkCyan);
+
+        public static Color DarkGoldenrod { get; } = Color.FromSKColor(SKColors.DarkGoldenrod);
 
         public static Color Red { get; } = Color.FromSKColor(SKColors.Red);
 
         public static Color DarkRed { get; } = Color.FromSKColor(SKColors.DarkRed);
 
+        public static Color DarkGreen { get; } = Color.FromSKColor(SKColors.DarkGreen);
+
+        public static Color DarkKhaki { get; } = Color.FromSKColor(SKColors.DarkKhaki);
+
+        public static Color DarkMagenta { get; } = Color.FromSKColor(SKColors.DarkMagenta);
+
+        public static Color DarkOliveGreen { get; } = Color.FromSKColor(SKColors.DarkOliveGreen);
+
+        public static Color DarkOrange { get; } = Color.FromSKColor(SKColors.DarkOrange);
+
+        public static Color DarkOrchid { get; } = Color.FromSKColor(SKColors.DarkOrchid);
+
+        public static Color DarkSalmon { get; } = Color.FromSKColor(SKColors.DarkSalmon);
+
+        public static Color DarkSeaGreen { get; } = Color.FromSKColor(SKColors.DarkSeaGreen);
+
+        public static Color DarkSlateBlue { get; } = Color.FromSKColor(SKColors.DarkSlateBlue);
+
+        public static Color DarkSlateGray { get; } = Color.FromSKColor(SKColors.DarkSlateGray);
+
+        public static Color DarkTurquoise { get; } = Color.FromSKColor(SKColors.DarkTurquoise);
+
+        public static Color DarkViolet { get; } = Color.FromSKColor(SKColors.DarkViolet);
+
+        public static Color DeepPink { get; } = Color.FromSKColor(SKColors.DeepPink);
+
+        public static Color DeepSkyBlue { get; } = Color.FromSKColor(SKColors.DeepSkyBlue);
+
+        public static Color DimGray { get; } = Color.FromSKColor(SKColors.DimGray);
+
+        public static Color DodgerBlue { get; } = Color.FromSKColor(SKColors.DodgerBlue);
+
+        public static Color Firebrick { get; } = Color.FromSKColor(SKColors.Firebrick);
+
+        public static Color FloralWhite { get; } = Color.FromSKColor(SKColors.FloralWhite);
+
+        public static Color ForestGreen { get; } = Color.FromSKColor(SKColors.ForestGreen);
+
+        public static Color Fuchsia { get; } = Color.FromSKColor(SKColors.Fuchsia);
+
+        public static Color Gainsboro { get; } = Color.FromSKColor(SKColors.Gainsboro);
+
+        public static Color GhostWhite { get; } = Color.FromSKColor(SKColors.GhostWhite);
+
+        public static Color Gold { get; } = Color.FromSKColor(SKColors.Gold);
+
+        public static Color Goldenrod { get; } = Color.FromSKColor(SKColors.Goldenrod);
+
         public static Color Green { get; } = Color.FromSKColor(SKColors.Green);
 
         public static Color GreenYellow { get; } = Color.FromSKColor(SKColors.GreenYellow);
+
+        public static Color Honeydew { get; } = Color.FromSKColor(SKColors.Honeydew);
+
+        public static Color HotPink { get; } = Color.FromSKColor(SKColors.HotPink);
+
+        public static Color IndianRed { get; } = Color.FromSKColor(SKColors.IndianRed);
+
+        public static Color Indigo { get; } = Color.FromSKColor(SKColors.Indigo);
+
+        public static Color Ivory { get; } = Color.FromSKColor(SKColors.Ivory);
+
+        public static Color Khaki { get; } = Color.FromSKColor(SKColors.Khaki);
+
+        public static Color Lavender { get; } = Color.FromSKColor(SKColors.Lavender);
+
+        public static Color LavenderBlush { get; } = Color.FromSKColor(SKColors.LavenderBlush);
+
+        public static Color LawnGreen { get; } = Color.FromSKColor(SKColors.LawnGreen);
+
+        public static Color LemonChiffon { get; } = Color.FromSKColor(SKColors.LemonChiffon);
+
+        public static Color LightBlue { get; } = Color.FromSKColor(SKColors.LightBlue);
+
+        public static Color LightCoral { get; } = Color.FromSKColor(SKColors.LightCoral);
+
+        public static Color LightCyan { get; } = Color.FromSKColor(SKColors.LightCyan);
+
+        public static Color LightGoldenrodYellow { get; } = Color.FromSKColor(SKColors.LightGoldenrodYellow);
+
+        public static Color LightGray { get; } = Color.FromSKColor(SKColors.LightGray);
+
+        public static Color LightGreen { get; } = Color.FromSKColor(SKColors.LightGreen);
+
+        public static Color LightPink { get; } = Color.FromSKColor(SKColors.LightPink);
+
+        public static Color LightSalmon { get; } = Color.FromSKColor(SKColors.LightSalmon);
+
+        public static Color LightSeaGreen { get; } = Color.FromSKColor(SKColors.LightSeaGreen);
+
+        public static Color LightSkyBlue { get; } = Color.FromSKColor(SKColors.LightSkyBlue);
+
+        public static Color LightSlateGray { get; } = Color.FromSKColor(SKColors.LightSlateGray);
+
+        public static Color LightSteelBlue { get; } = Color.FromSKColor(SKColors.LightSteelBlue);
+
+        public static Color LightYellow { get; } = Color.FromSKColor(SKColors.LightYellow);
+
+        public static Color Lime { get; } = Color.FromSKColor(SKColors.Lime);
 
         public static Color Gray { get; } = Color.FromSKColor(SKColors.Gray);
 
@@ -22,7 +166,125 @@ namespace DrawnUi.Draw
 
         public static Color Orange { get; } = Color.FromSKColor(SKColors.Orange);
 
-        public static Color LimeGreen { get; } = Color.FromSKColor(SKColors.LawnGreen);
+        public static Color LimeGreen { get; } = Color.FromSKColor(SKColors.LimeGreen);
+
+        public static Color Linen { get; } = Color.FromSKColor(SKColors.Linen);
+
+        public static Color Magenta { get; } = Color.FromSKColor(SKColors.Magenta);
+
+        public static Color Maroon { get; } = Color.FromSKColor(SKColors.Maroon);
+
+        public static Color MediumAquamarine { get; } = Color.FromSKColor(SKColors.MediumAquamarine);
+
+        public static Color MediumBlue { get; } = Color.FromSKColor(SKColors.MediumBlue);
+
+        public static Color MediumOrchid { get; } = Color.FromSKColor(SKColors.MediumOrchid);
+
+        public static Color MediumPurple { get; } = Color.FromSKColor(SKColors.MediumPurple);
+
+        public static Color MediumSeaGreen { get; } = Color.FromSKColor(SKColors.MediumSeaGreen);
+
+        public static Color MediumSlateBlue { get; } = Color.FromSKColor(SKColors.MediumSlateBlue);
+
+        public static Color MediumSpringGreen { get; } = Color.FromSKColor(SKColors.MediumSpringGreen);
+
+        public static Color MediumTurquoise { get; } = Color.FromSKColor(SKColors.MediumTurquoise);
+
+        public static Color MediumVioletRed { get; } = Color.FromSKColor(SKColors.MediumVioletRed);
+
+        public static Color MidnightBlue { get; } = Color.FromSKColor(SKColors.MidnightBlue);
+
+        public static Color MintCream { get; } = Color.FromSKColor(SKColors.MintCream);
+
+        public static Color MistyRose { get; } = Color.FromSKColor(SKColors.MistyRose);
+
+        public static Color Moccasin { get; } = Color.FromSKColor(SKColors.Moccasin);
+
+        public static Color NavajoWhite { get; } = Color.FromSKColor(SKColors.NavajoWhite);
+
+        public static Color Navy { get; } = Color.FromSKColor(SKColors.Navy);
+
+        public static Color OldLace { get; } = Color.FromSKColor(SKColors.OldLace);
+
+        public static Color Olive { get; } = Color.FromSKColor(SKColors.Olive);
+
+        public static Color OliveDrab { get; } = Color.FromSKColor(SKColors.OliveDrab);
+
+        public static Color OrangeRed { get; } = Color.FromSKColor(SKColors.OrangeRed);
+
+        public static Color Orchid { get; } = Color.FromSKColor(SKColors.Orchid);
+
+        public static Color PaleGoldenrod { get; } = Color.FromSKColor(SKColors.PaleGoldenrod);
+
+        public static Color PaleGreen { get; } = Color.FromSKColor(SKColors.PaleGreen);
+
+        public static Color PaleTurquoise { get; } = Color.FromSKColor(SKColors.PaleTurquoise);
+
+        public static Color PaleVioletRed { get; } = Color.FromSKColor(SKColors.PaleVioletRed);
+
+        public static Color PapayaWhip { get; } = Color.FromSKColor(SKColors.PapayaWhip);
+
+        public static Color PeachPuff { get; } = Color.FromSKColor(SKColors.PeachPuff);
+
+        public static Color Peru { get; } = Color.FromSKColor(SKColors.Peru);
+
+        public static Color Pink { get; } = Color.FromSKColor(SKColors.Pink);
+
+        public static Color Plum { get; } = Color.FromSKColor(SKColors.Plum);
+
+        public static Color PowderBlue { get; } = Color.FromSKColor(SKColors.PowderBlue);
+
+        public static Color Purple { get; } = Color.FromSKColor(SKColors.Purple);
+
+        public static Color RosyBrown { get; } = Color.FromSKColor(SKColors.RosyBrown);
+
+        public static Color RoyalBlue { get; } = Color.FromSKColor(SKColors.RoyalBlue);
+
+        public static Color SaddleBrown { get; } = Color.FromSKColor(SKColors.SaddleBrown);
+
+        public static Color Salmon { get; } = Color.FromSKColor(SKColors.Salmon);
+
+        public static Color SandyBrown { get; } = Color.FromSKColor(SKColors.SandyBrown);
+
+        public static Color SeaGreen { get; } = Color.FromSKColor(SKColors.SeaGreen);
+
+        public static Color SeaShell { get; } = Color.FromSKColor(SKColors.SeaShell);
+
+        public static Color Sienna { get; } = Color.FromSKColor(SKColors.Sienna);
+
+        public static Color Silver { get; } = Color.FromSKColor(SKColors.Silver);
+
+        public static Color SkyBlue { get; } = Color.FromSKColor(SKColors.SkyBlue);
+
+        public static Color SlateBlue { get; } = Color.FromSKColor(SKColors.SlateBlue);
+
+        public static Color SlateGray { get; } = Color.FromSKColor(SKColors.SlateGray);
+
+        public static Color Snow { get; } = Color.FromSKColor(SKColors.Snow);
+
+        public static Color SpringGreen { get; } = Color.FromSKColor(SKColors.SpringGreen);
+
+        public static Color SteelBlue { get; } = Color.FromSKColor(SKColors.SteelBlue);
+
+        public static Color Tan { get; } = Color.FromSKColor(SKColors.Tan);
+
+        public static Color Teal { get; } = Color.FromSKColor(SKColors.Teal);
+
+        public static Color Thistle { get; } = Color.FromSKColor(SKColors.Thistle);
+
+        public static Color Tomato { get; } = Color.FromSKColor(SKColors.Tomato);
+
+        public static Color Turquoise { get; } = Color.FromSKColor(SKColors.Turquoise);
+
+        public static Color Violet { get; } = Color.FromSKColor(SKColors.Violet);
+
+        public static Color Wheat { get; } = Color.FromSKColor(SKColors.Wheat);
+
+        public static Color WhiteSmoke { get; } = Color.FromSKColor(SKColors.WhiteSmoke);
+
+        public static Color Yellow { get; } = Color.FromSKColor(SKColors.Yellow);
+
+        public static Color YellowGreen { get; } = Color.FromSKColor(SKColors.YellowGreen);
 
         public static Color Default
         {
